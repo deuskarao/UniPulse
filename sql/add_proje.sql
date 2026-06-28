82ec824ea4b1988533aa7eeef1f65516de847897
Muhammed Mustafa Kara <119631661+deuskarao@users.noreply.github.com>
2026-06-28 18:00:11 +0300

    Delete sql directory

--- sql/add_proje.sql
+++
@@ -1,5 +0,0 @@
--- `department_courses` tablosuna `proje_yuzde` eklenecek
-ALTER TABLE public.department_courses ADD COLUMN IF NOT EXISTS proje_yuzde numeric DEFAULT 0;
-
--- `student_grades` tablosuna `proje` notu eklenecek
-ALTER TABLE public.student_grades ADD COLUMN IF NOT EXISTS proje numeric;
